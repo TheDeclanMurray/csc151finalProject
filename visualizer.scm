8fc8de2548a6ffb5d112e57610dec31a1f9df9a0
jaysonkunkel <jayson.kunkel@gmail.com>
2022-12-16 13:43:57 -0600

    hi

--- visualizer.scm
+++ visualizer.scm
@@ -1,19 +1,28 @@
-(import music)
 (import canvas)
+(import html)
 
 (define width 1800)
 (define height 300)
 (define c1 (canvas width height))
-(define c2 (canvas width height))
+
+;;; (reset canv) -> void?
+;;;   canv : canvas?
+;;; "Resets" the given canvas by creating a blank rectangle with the same dimensions of the canvas.
 (define reset
   (lambda (canv)
-    (rectangle canv 0 0 width height "solid" "white")))
+    (rectangle canv 0 0 width height "solid" "gray")))
 
-
+;;; (getRandom n) -> integer?
+;;;   n : integer?, n >= 0
+;;; Returns the result of adding 50 to a random integer between 0 and n, exclusive.
 (define getRandom
   (lambda (n)
     (+ 50 (random n))))
 
+;;; (randomVec n) -> vector?
+;;;   n : integer?
+;;; Creates a vector of length n and fills it with random integers between 50 and 300, 
+;;; exclusive, using the getRandom function.
 (define randomVec
   (lambda (n)
     (let([vec (vector-range n)])
@@ -24,91 +33,62 @@
           vec)
         vec))))
 
-(define getFrequency
-  (lambda (x)
-    (* 440 (expt 2 (/ (- x 69) 12)))))
-
-(define make-rectangles
+;;; (makeVisualizer vec canv) -> void?
+;;;   vec : vector?, a vector of integers
+;;;   canv : canvas?
+;;; Draws (vector-length vec) solid black rectangles on the given canvas, each separated by
+;;; 2 pixels. Each rectangle has width 23 and height of the corresponding element in vec.
+(define makeVisualizer
   (lambda (vec canv)
     (let ([count (vector 1)])
       (vector-map
         (lambda (h)
           (begin
-            (rectangle canv (* 25 (vector-ref count 0)) (- height h) 24 h "solid" "black")
-            (vector-set! count 0 (+ 1 (vector-ref count 0))))) vec))))
+            (rectangle canv (* 25 (vector-ref count 0)) (- height h) 23 h "solid" "black")
+            (vector-set! count 0 (+ 1 (vector-ref count 0))))) 
+        vec))))
 
-(define testVec (make-vector 10 100))
-
-; (define make-rectangles-2
-;   (lambda (vec canv)
-;     (let ([count (vector 1)])
-;       (vector-map
-;         (lambda (h)
-;           (begin
-;             (rectangle canv (* 25 (vector-ref count 0)) (- height h) 24 h "solid" "black")
-;             (vector-set! count 0 (+ 1 (vector-ref count 0))))) vec))))
 
   
   
   (define lastFrame (vector 0))
+
+;;; (getLastFrame) -> integer?
+;;; Returns the time of the last visual update, in milliseconds.
   (define getLastFrame
-    (lambda () (vector-ref lastFrame 0)))
+    (lambda () 
+      (vector-ref lastFrame 0)))
+
+;;; (setLastFrame time) -> void?
+;;;   time : integer?, time in milliseconds
+;;; Sets the time of the last visual update to the given time.
   (define setLastFrame
-    (lambda (time) (vector-set! lastFrame 0 time)))
+    (lambda (time) 
+      (vector-set! lastFrame 0 time)))
   
-  (define animation-testing
-    (lambda (t)
-      (if (> (getLastFrame) t)
-        void
-        
-        (begin
-          (setLastFrame (+ t 500))
-          (reset c2)
-          (make-rectangles (randomVec 20) c2)))))
+;;; (animateVisualizer canv) -> void?
+;;;   canv : canvas?
+;;; If getLastFrame is > time, t, returns void. Otherwise, resets the given canvas and
+;;; calls makeVisualizer once every 500 milliseconds, or half-second, with 20 rectangles. 
+  (define animateVisualizer
+    (lambda (canv)
+      (lambda (t)
+        (if (> (getLastFrame) t)
+          void
+          (begin
+            (setLastFrame (+ t 500))
+            (reset canv)
+            (makeVisualizer (randomVec 20) canv))))))
+
+(animate-with (animateVisualizer c1))
+c1
   
-  (animate-with animation-testing)
-  c2
+; (define startVisualizerBtn (button "button1" "click to start"))
 
+; (button-onclick startVisualizerBtn 
+;   (lambda ()
+;       (begin
+;       (animate-with (animateVisualizer c1)))))
 
-(define snareDrum (getFrequency 38))
-snareDrum
-
-(define numSamples (* snareDrum 0.5))
-numSamples
-
-(define freqTest
-  (vector-map
-  (lambda (h)
-    (floor (* 50 h )))(vector-map abs (vector-range -1 1 (/ 1 36)))))
-
-freqTest
-
-(define c3 (canvas width height))
-(reset c3)
-
-; (make-rectangles freqTest c3)
-; c3
-
-
-; (define test (tri (vector-range 0 30) (/ 30 73.4)))
-
-; (define testMap
-;   (vector-map 
-;   (lambda (i)
-;     (abs (floor (* 150 i)))) test))
-
-; (make-rectangles testMap c3)
-; c3
-  
-
-  
-(define storage (list->vector (reverse (vector->list (vector-range 1 30)))))
-storage
-
-(define storageMap
-  (vector-map
-    (lambda (x)
-      (* 10 x)) storage))
-
-; (make-rectangles storageMap c3)
-; c3
+; startVisualizerBtn
+; c1
